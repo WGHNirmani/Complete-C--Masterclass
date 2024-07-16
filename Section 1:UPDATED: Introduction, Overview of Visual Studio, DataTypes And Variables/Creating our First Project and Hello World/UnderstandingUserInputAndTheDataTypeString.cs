0e4906537b8c37e680f60692ec463c4d63648c65
Hirunika Nirmani <hirunikanirmani26@gmail.com>
2024-07-16 07:34:20 +0530

    Create UnderstandingUserInputAndTheDataTypeString.cs

---
+++ Section 1:UPDATED: Introduction, Overview of Visual Studio, DataTypes And Variables/Creating our First Project and Hello World/UnderstandingUserInputAndTheDataTypeString.cs
@@ -0,0 +1,11 @@
+// Prints out whatever is in side of ()
+Console.WriteLine("Hello World!");
+
+// takes the user input and stores it
+// Variable with the name "userInput" and the data type 'string"
+// string are used for text
+string userInput = Console.ReadLine();
+
+// we are adding to string together and writing them onto the console
+Console.WriteLine("You entered " + userInput);
+Console.ReadKey();

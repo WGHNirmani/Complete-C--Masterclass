750bee38cf860356fe77ab073fa0f4016e52f3a6
Hirunika Nirmani <hirunikanirmani26@gmail.com>
2024-07-23 09:49:37 +0530

    Create UsingTheParseMethodToConvertFromStringToInt.cs

---
+++ Section 1:UPDATED: Introduction, Overview of Visual Studio, DataTypes And Variables/Creating our First Project and Hello World/UsingTheParseMethodToConvertFromStringToInt.cs
@@ -0,0 +1,21 @@
+// See https://aka.ms/new-console-template for more information
+
+// Prints out whatever is in side of ()
+Console.WriteLine("Enter a whole number!");
+
+// datatype variableName = initial value;
+int myNumber = 0;
+
+
+
+
+// takes the user input and stores it
+// Variable with the name "userInput" and the data type "string"
+// strings are used for text 
+string userInput = Console.ReadLine();
+//myNumber = int.Parse(Console.ReadLine());
+myNumber = int.Parse(userInput);
+
+// we are adding to strings together and writing them onto the console
+Console.WriteLine("You entered " + userInput);
+Console.ReadKey();

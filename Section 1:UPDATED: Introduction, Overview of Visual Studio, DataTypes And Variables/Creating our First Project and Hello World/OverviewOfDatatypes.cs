5007725fd0bd475fdab2b8e766e0df1da323d186
Hirunika Nirmani <hirunikanirmani26@gmail.com>
2024-07-23 09:19:24 +0530

    Create OverviewOfDatatypes.cs

---
+++ Section 1:UPDATED: Introduction, Overview of Visual Studio, DataTypes And Variables/Creating our First Project and Hello World/OverviewOfDatatypes.cs
@@ -0,0 +1,14 @@
+// Reference Type
+string myName = "Denis";
+
+// Value Types
+int myAge = 35;
+double pi = - 3.14;
+
+byte age = 255;
+
+short linkedinConnections = 32550;
+
+long phoneNumber = 017657723991;
+
+float pi = 3.14f;

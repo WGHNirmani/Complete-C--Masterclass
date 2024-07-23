ee4a0a9be68083361b8b7591d54f3d22fcf3ccfa
Hirunika Nirmani <hirunikanirmani26@gmail.com>
2024-07-23 10:02:24 +0530

    Create FinalizingOurAdditionCalculator.cs

---
+++ Section 1:UPDATED: Introduction, Overview of Visual Studio, DataTypes And Variables/Creating our First Project and Hello World/FinalizingOurAdditionCalculator.cs
@@ -0,0 +1,27 @@
+// See https://aka.ms/new-console-template for more information
+
+
+
+// datatype variableName = initial value;
+int myNumber = 0;
+int myNumber2 = 0;
+
+// Prints out whatever is in side of ()
+Console.WriteLine("Enter a whole number!");
+
+// takes the user input and stores it
+// Variable with the name "userInput" and the data type "string"
+// strings are used for text 
+string userInput = Console.ReadLine();
+//myNumber = int.Parse(Console.ReadLine());
+myNumber = int.Parse(userInput);
+
+Console.WriteLine("Enter a whole number!");
+userInput = Console.ReadLine();
+myNumber2 = int.Parse(userInput);
+
+int sum = myNumber + myNumber2;
+
+// we are adding to strings together and writing them onto the console
+Console.WriteLine("The result of " + myNumber + " and " + myNumber2 + " is " + sum);
+Console.ReadKey();
